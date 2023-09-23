7ebeffd9ce187c9a46d59f6d695254c92f39c7fa
Marco André <marcoandre2103@gmail.com>
2023-09-23 16:51:44 +0100

    All extracted!

--- scrapper.py
+++ scrapper.py
@@ -2,9 +2,10 @@ import pandas as pd
 import os
 from bs4 import BeautifulSoup
 import re
+from collections import defaultdict
 
 df = pd.DataFrame(columns=['Name', 'Kingdom', 'Phylum',
-                  'Class', 'Order', 'Family', 'Genus', 'Scientific Name', 'Quote', 'Text'])
+                  'Class', 'Order', 'Family', 'Genus', 'Scientific Name', 'Quote', 'Text', 'Pairs'])
 
 
 def parser():
@@ -18,9 +19,8 @@ def parser():
         if os.path.isfile(file_path):
             with open(file_path, 'r') as file:
                 file_contents = file.read()
+                print(f"Trying to parse {file_name}")
                 file_parser(file_contents, file_name)
-                print(f"File {file_name} parsed.")
-        
 
 
 def add_row(new_row):
@@ -34,13 +34,20 @@ def save_csv():
     df.to_csv('output.csv', index=False)
 
 
+count_dict = defaultdict(int)
+
+count_errors = 0
+
+
 def file_parser(content, file_name):
+    global count_errors
 
     soup = BeautifulSoup(content, 'html.parser')
     try:
         quote = soup.find('blockquote').find('p').text.strip()
     except:
         quote = ''
+        count_errors += 1
 
     try:
         taxum = soup.find('dl', attrs={
@@ -49,6 +56,12 @@ def file_parser(content, file_name):
             taxum[i] = taxum[i].text.strip()
     except:
         taxum = []
+        count_errors += 1
+
+    # if no taxum found, append empty strings
+    if len(taxum) != 7:
+        for i in range(7-len(taxum)):
+            taxum.append('')
 
     try:
         text = soup.find('div', attrs={'id': 'single-animal-text'}).findAll(
@@ -57,6 +70,7 @@ def file_parser(content, file_name):
             text[i] = text[i].text.strip()
     except:
         text = []
+        count_errors += 1
 
     pattern = re.compile(r'\s+')
     cleaned_text = [pattern.sub(' ', s.strip())
@@ -64,10 +78,26 @@ def file_parser(content, file_name):
 
     combined_text = ' '.join(cleaned_text)
 
-    # if no taxum found, append empty strings
-    if len(taxum) != 7:
-        for i in range(7-len(taxum)):
-            taxum.append('')
+    pairs = []
+
+    try:
+        factsBox = soup.findAll(
+            'div', attrs={'class': 'row animal-facts-box'})[1]
+        dt_elements = factsBox.findAll('dt', class_='col-sm-6 text-md-right')
+        dd_elements = factsBox.findAll('dd', class_='col-sm-6')
+
+        for dt, dd in zip(dt_elements, dd_elements):
+            dt_text = dt.text.strip()  # Extract and clean text from <dt>
+            dd_text = dd.text.strip()  # Extract and clean text from <dd>
+            global count_dict
+            count_dict[dt_text] += 1
+            # print(count_dict)
+            pairs.append((dt_text, dd_text))
+
+    except:
+        count_errors += 1
+
+    # print(pairs)
 
     new_row = {
         'Name': file_name.split('.')[0],
@@ -79,11 +109,68 @@ def file_parser(content, file_name):
         'Genus': taxum[5],
         'Scientific Name': taxum[6],
         'Quote': quote,
-        'Text': combined_text
+        'Text': combined_text,
+        'Pairs': pairs,
     }
 
     add_row(new_row)
 
 
-parser()
+# parser()
 save_csv()
+# print(count_dict)
+print(f"Number of errors: {count_errors}")
+
+"""
+Diet: 2519
+Color: 2380
+Skin Type: 2363
+Fun Fact: 2352
+Lifespan: 2147
+Weight: 1843
+Habitat: 1692
+Common Name: 1587
+Length: 1582
+Most Distinctive Feature: 1541
+Group Behavior: 1493
+Predators: 1413
+Prey: 1408
+Biggest Threat: 1336
+Name Of Young: 1247
+Lifestyle: 1232
+Type: 1182
+Location: 1155
+Other Name(s): 1100
+Number Of Species: 1076
+Favorite Food: 981
+Estimated Population Size: 946
+Age of Sexual Maturity: 859
+Gestation Period: 810
+Aggression: 809
+Venomous: 784
+Litter Size: 781
+Main Prey: 771
+Top Speed: 755
+Distinctive Feature: 735
+Slogan: 698
+Height: 617
+Average Litter Size: 600
+Temperament: 542
+Group: 539
+Incubation Period: 440
+Nesting Location: 439
+Wingspan: 407
+Age of Weaning: 402
+Origin: 360
+Special Features: 263
+Average Clutch Size: 261
+Migratory: 222
+Age Of Independence: 210
+Age of Molting: 182
+Age Of Fledgling: 170
+Average Spawn Size: 170
+Diet for this Fish: 160
+Optimum pH Level: 157
+Training: 117
+Optimum PH Level: 3
+"""
